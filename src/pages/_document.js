fd23bb92c3ff12228179beffedba6b7e2166a19d
thinceller <kohei.ct@icloud.com>
2020-01-20 03:43:37 +0900

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,9 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/icon/icon-192x192.png" />
+          <link rel="manifest" href="/manifest.json" />
           <style>{`body{margin:0;}`}</style>
         </Head>
         <body>
