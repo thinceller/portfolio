0ce9a4ab842b87955d2fed27713c43a038c072ea
thinceller <kohei.ct@icloud.com>
2020-01-20 03:03:23 +0900

    Add image for og:image

--- src/config/blog.js
+++ src/config/blog.js
@@ -1,4 +1,4 @@
 export const blogTitle = 'thinceller'
 export const blogRootUrl = 'https://thinceller.dev'
 export const blogDescription = "thinceller's blog and portfolio site"
-export const logoUrl = 'https://thinceller.dev/images/logo.png'
+export const logoUrl = 'https://thinceller.dev/images/logo-og.png'
